8508109ce128bef6e63dce776d1162e7872ccb65
turntayble81 <turntayble81@gmail.com>
2018-05-28 16:54:16 -0400

    Got it working. Added comments. Misc cleanup

--- addressIdentifier.js
+++ addressIdentifier.js
@@ -1,8 +1,9 @@
 #!/usr/local/bin/node
 
-const readline = require('readline');
-const fs       = require('fs');
-const config   = require('./config');
+const fs              = require('fs');
+const async           = require('async');
+const inputFileParser = require('./lib/inputFileProcessor');
+const checkAddresses  = require('./lib/checkAddresses');
 
 //Get path of input file
 const inputFile = process.argv[2];
@@ -12,68 +13,22 @@ if(!inputFile) {
     process.exit(1);
 }
 
-const googleMapsClient = require('@google/maps').createClient({
-    key: config.googleAPIKey
-});
+async.waterfall([
 
-//TODO: check if file exists
+    //Check if specified input file exists
+    (next)=> fs.stat(inputFile, next),
 
-function loadInputFile(inputFile, cb) {
-    const inputFileReader = readline.createInterface({
-        input: fs.createReadStream(inputFile)
+    //Process input file
+    (stat, next) => inputFileParser(inputFile, next),
+
+    //Filter addresses to those contained within specified region
+    (inputData, next) => checkAddresses(inputData, next),
+
+], (err, withinRegionArr=[])=> {
+    if(err) {
+        return console.error(err);
+    }
+    withinRegionArr.forEach((address) => {
+        console.log(address);
     });
-
-    let inputData = {
-        coordinates : '',
-        addresses   : []
-    };
-
-    let lineIdx = 0;
-    inputFileReader.on('line', function (line) {
-        if(lineIdx == 0) {
-            inputData.coordinates = line;
-        }else {
-            inputData.addresses.push(line);
-        }
-        lineIdx++;
-    });
-
-    inputFileReader.on('close', () => {
-        if(!inputData.coordinates || !inputData.addresses.length) {
-            return cb('Input file is malformed');
-        }
-
-        inputData.coordinates = inputData.coordinates
-            .split(',')
-            .map((point) => {
-                point = point.replace(/[^\d.-]/g, '');
-                return parseFloat(point);
-            });
-
-        let coordinateArr = [];
-        for(let n=0; n< inputData.coordinates.length; n+=2) {
-            coordinateArr.push([
-                inputData.coordinates[n],
-                inputData.coordinates[n+1]
-            ]);
-        }
-        inputData.coordinates = coordinateArr;
-        cb(null, inputData);
-    });
-}
-
-function checkAddresses({coordinates, addresses}=args={}, cb) {
-    googleMapsClient.geocode({
-        address: '1600 Amphitheatre Parkway, Mountain View, CA'
-    }, (err, response) => {
-        if(!err) {
-            console.dir(response.json.results[0].geometry.location, {depth: null});
-        }
-    });
-}
-
-loadInputFile(inputFile, (err, inputData) => {
-    console.dir(err)
-    console.dir(inputData);
-    checkAddresses();
 });
